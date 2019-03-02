29f807bda4f49f6a2c6fc7bb4c830ae0114ed0ce
Krzychu88 <wojcieszek.krzysztof@gmail.com>
2019-03-02 13:40:43 +0100

    dodalem drugiego souta

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("coś inego niż tamten");
         System.out.println("Praca w grupie");
 
         for (int i = 0; i <= 100; i++) {
