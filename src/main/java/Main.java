b1986a9ab0164098ee8541af7931886a999f226b
Krzychu88 <wojcieszek.krzysztof@gmail.com>
2019-03-02 13:30:30 +0100

    Dodanie petli for

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Praca w grupie");
+
+        for (int i = 0; i <100; i++) {
+            System.out.println("Wartość iteratora to " + i);
+
+        }
     }
 }
