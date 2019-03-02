8da9377a343343439d27e264df26b719b5299941
staniukiewicz <staniukiewicz@gmail.com>
2019-03-02 13:37:54 +0100

    poprawiłem co zjebałeś i dodałem więcej

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Praca w grupie");
 
-        for (int i = 0; i <100; i++) {
+        for (int i = 0; i <= 100; i++) {
             System.out.println("Wartość iteratora to " + i);
 
+        }
+        System.out.println();
+
+        for (int i = 100; i >= 0; --i) {
+            System.out.println("Wartość iteratora to " + i);
         }
     }
 }
